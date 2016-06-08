f46a508ee3a24645c564e6f396b619d86d42cf3c
Nived <nivedn3>
2016-06-08 15:31:46 +0530

    Echo

---
+++ run.py
@@ -0,0 +1,20 @@
+from yowsup.stacks import  YowStackBuilder
+from layer import EchoLayer
+from yowsup.layers.auth import AuthError
+from yowsup.layers import YowLayerEvent
+from yowsup.layers.network import YowNetworkLayer
+from yowsup.env import YowsupEnv
+
+credentials = ("918289905800", "mpFhXTO6T1aB0SmHh9nsCPk/39Q=") # replace with your phone and password
+
+if __name__==  "__main__":
+    stackBuilder = YowStackBuilder()
+
+    stack = stackBuilder\
+        .pushDefaultLayers(True)\
+        .push(EchoLayer)\
+        .build()
+
+    stack.setCredentials(credentials)
+    stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))   #sending the connect signal
+    stack.loop() #this is the program mainloop
